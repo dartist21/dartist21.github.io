c7268d1ab671f232f01a908ea81df725fc88cfa3
Denys <dartist21@gmail.com>
2017-07-08 05:29:08 +0300

    new version

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-(function () {
+(function() {
 
   class Form {
     constructor(data) {
@@ -7,7 +7,9 @@
       this.username = this.data.username;
       this.email = this.data.email;
       this.city = this.data.city;
+      this.cityValue = this.data.cityValue;
       this.country = this.data.country;
+      this.countryValue = this.data.countryValue;
       this.fb = this.data.fb;
       this.vk = this.data.vk;
       this.tw = this.data.tw;
@@ -15,16 +17,32 @@
       this.catUrl = this.data.catUrl;
     }
   
-    setInformation (name, arg) {
-        this[name] = arg;
+    setInformation(name, arg) {
+      this[name] = arg;
+    }
+  
+    resetAll() {
+      this.username = null;
+      this.email = null;
+      this.city = null;
+      this.cityValue = null;
+      this.country = null;
+      this.countryValue = null;
+      this.fb = null;
+      this.vk = null;
+      this.tw = null;
+      this.ok = null;
+      this.catUrl = null;
     }
   }
   
   Form.defaultData = {
     username: null,
     email: null,
-    city: 'Kyiv',
-    country: 'Ukraine',
+    city: null,
+    cityValue: null,
+    country: null,
+    countryValue: null,
     fb: null,
     vk: null,
     tw: null,
@@ -32,110 +50,138 @@
     catUrl: null
   };
   
-  let questionnaire = new Form();
+  var questionnaire = new Form();
   
       function pagination(curStep) {
-    let pagItems = document.querySelectorAll('.pagination-pg__item');
-    let prev = document.querySelector('.pagination-btn__prev');
-    let next = document.querySelector('.pagination-btn__next');
-    let finish = document.querySelector('.pagination-btn__finish');
+      let pagItems = document.querySelectorAll('.pagination-pg__item');
+      let prev = document.querySelector('.pagination-btn__prev');
+      let next = document.querySelector('.pagination-btn__next');
+      let finish = document.querySelector('.pagination-btn__finish');
   
-    for (let i = 0; i < pagItems.length; i++) {
-      pagItems[i].addEventListener('click', changePage);
-    }
+      let validationError = rootElement.querySelector('#validation-error');
   
-    if (prev) {
-      prev.addEventListener('click', goPrev);
-    }
-    if (next) {
-      next.addEventListener('click', goNext);
-    }
-    if (finish) {
-      finish.addEventListener('click', goFinish);
-    }
+      for (let i = 0; i < pagItems.length; i++) {
+        pagItems[i].addEventListener('click', changePage);
+      }
+  
+      if (prev) {
+        prev.addEventListener('click', goPrev);
+      }
+      if (next) {
+        next.addEventListener('click', goNext);
+      }
+      if (finish) {
+        finish.addEventListener('click', goFinish);
+      }
   
   
-    switch (curStep) {
-      case 1:
-        prev.classList.add('button--not-active');
-        for (let i = curStep; i < pagItems.length; i++) {
-          pagItems[i].classList.add('button--not-active');
-        }
-        break;
-      case 2:
-        for (let i = curStep - 2; i >= 0; --i) {
-          pagItems[i].classList.add('button--is-active');
-        }
-        for (let i = curStep; i < pagItems.length; i++) {
-          pagItems[i].classList.add('button--not-active');
-        }
-        break;
-      case 3:
-        for (let i = curStep - 2; i >= 0; --i) {
-          pagItems[i].classList.add('button--is-active');
-        }
-        for (let i = curStep; i < pagItems.length; i++) {
-          pagItems[i].classList.add('button--not-active');
-        }
-        break;
-      case 4:
-        for (let i = curStep - 2; i >= 0; --i) {
-          pagItems[i].classList.add('button--is-active');
-        }
-        for (let i = curStep; i < pagItems.length; i++) {
-          pagItems[i].classList.add('button--not-active');
-        }
-        next.classList.add('button--display-none');
-        finish.classList.remove('button--display-none');
-        break;
-      case finish:
-  
+      switch (curStep) {
+        case 1:
+          prev.classList.add('button--not-active');
+          for (let i = curStep; i < pagItems.length; i++) {
+            pagItems[i].classList.add('button--not-active');
+          }
           break;
-      default:
-        console.log('check pagination');
+        case 2:
+          for (let i = curStep - 2; i >= 0; --i) {
+            pagItems[i].classList.add('button--is-active');
+          }
+          for (let i = curStep; i < pagItems.length; i++) {
+            pagItems[i].classList.add('button--not-active');
+          }
+          break;
+        case 3:
+          for (let i = curStep - 2; i >= 0; --i) {
+            pagItems[i].classList.add('button--is-active');
+          }
+          for (let i = curStep; i < pagItems.length; i++) {
+            pagItems[i].classList.add('button--not-active');
+          }
+          break;
+        case 4:
+          for (let i = curStep - 2; i >= 0; --i) {
+            pagItems[i].classList.add('button--is-active');
+          }
+          for (let i = curStep; i < pagItems.length; i++) {
+            pagItems[i].classList.add('button--not-active');
+          }
+          next.classList.add('button--display-none');
+          finish.classList.remove('button--display-none');
+          break;
+        default:
+          console.log('check pagination');
+      }
   
+      function changePage(e) {
+        let pagItem = e.target;
+        let pagItemNum = pagItem.dataset.value;
+        page.redirect('/step' + pagItemNum);
+      }
+  
+      function goPrev() {
+        prevStep = --curStep;
+        page.redirect('/step' + prevStep);
+      }
+  
+      function goNext() {
+        if (validate()) {
+          nextStep = ++curStep;
+          page.redirect('/step' + nextStep);
+        } else {
+          validationError.classList.remove('form__error--display-none');
+        }
+      }
+  
+      function goFinish() {
+        if (questionnaire.catUrl) {
+          page.redirect('/final');
+        } else {
+          validationError.classList.remove('form__error--display-none');
+        }
+      }
+  
+      function validate() {
+        switch (curStep) {
+          case 1:
+            if (questionnaire.username && questionnaire.email) {
+              return true;
+            }
+            break;
+          case 2:
+            if (questionnaire.country && questionnaire.city) {
+              return true;
+            }
+            break;
+          case 3:
+            if (questionnaire.fb || questionnaire.vk || questionnaire.tw || questionnaire.ok) {
+              return true;
+            }
+            break;
+          default:
+            console.log('check pagination');
+        }
+      }
     }
-  
-    function changePage(e) {
-      let pagItem = e.target;
-      let pagItemNum = pagItem.dataset.value;
-      page.redirect('/step' + pagItemNum);
-    }
-  
-    function goPrev() {
-      prevStep = --curStep;
-      page.redirect('/step' + prevStep);
-    }
-  
-    function goNext() {
-      nextStep = ++curStep;
-      page.redirect('/step' + nextStep);
-    }
-  
-    function goFinish() {
-      page.redirect('/final');
-    }
-  
-  
-  }
   
   function notfound() {
     console.log('notfound');
-      rootElement.innerHTML = templates['404']();
+    rootElement.innerHTML = templates['404']();
   }
   
     function final() {
     rootElement.innerHTML = templates['final']();
   
-    nameEl =      rootElement.querySelector('.card-name');
-    emailEl =     rootElement.querySelector('.card-email');
-    locationEl =  rootElement.querySelector('.card-location');
-    fbEl =        rootElement.querySelector('.card-fb');
-    vkEl =        rootElement.querySelector('.card-vk');
-    twEl =        rootElement.querySelector('.card-tw');
-    okEl =        rootElement.querySelector('.card-ok');
-    catEl =       rootElement.querySelector('.card-img');
+    let resetBtn = rootElement.querySelector('.pagination-btn__reset');
+    resetBtn.addEventListener('click', resetForm);
   
+    nameEl =      rootElement.querySelector('.profile-card__name');
+    emailEl =     rootElement.querySelector('.profile-card__email');
+    locationEl =  rootElement.querySelector('.profile-card__location');
+    fbEl =        rootElement.querySelector('.profile-card__fb');
+    vkEl =        rootElement.querySelector('.profile-card__vk');
+    twEl =        rootElement.querySelector('.profile-card__tw');
+    okEl =        rootElement.querySelector('.profile-card__ok');
+    catEl =       rootElement.querySelector('.profile-card__image');
   
     nameEl.innerHTML = questionnaire.username;
     emailEl.innerHTML = questionnaire.email;
@@ -155,6 +201,12 @@
     }
   
     catEl.src = questionnaire.catUrl;
+  
+    function resetForm() {
+      questionnaire.resetAll();
+      page.redirect('/step1');
+    }
+  
   }
   
     function step1() {
@@ -166,11 +218,16 @@
     let nameInput = rootElement.querySelector('.form__name');
     let emailInput = rootElement.querySelector('.form__email');
   
-    console.log(nameInput.value);
+    let nameError = rootElement.querySelector('#name-error');
+    let emailError = rootElement.querySelector('#email-error');
   
+    if (questionnaire.username) {
+      nameInput.value = questionnaire.username;
+    }
   
-    let nameError = rootElement.querySelector('#name-error')
-    let emailError = rootElement.querySelector('#email-error')
+    if (questionnaire.email) {
+      emailInput.value = questionnaire.email;
+    }
   
     nameInput.addEventListener('focusin', resetStateName);
     emailInput.addEventListener('focusin', resetStateEmail);
@@ -213,6 +270,7 @@
       nameError.classList.add('form__error--hidden');
       el.target.classList.remove('form__input--not-valid');
     }
+  
   }
   
     function step2() {
@@ -221,36 +279,93 @@
     let curStep = 2;
     pagination(curStep);
   
-    const cityData  = fetch('/js/json/cities.json');
-    cityData
-    .then(response => {
-      return response.json();
-     })
-    .then(data => {
-      // console.log(data);
+    let countryInput = rootElement.querySelector('.form__country');
+    let cityInput = rootElement.querySelector('.form__city');
   
-    })
-    .catch(error => {
-      console.log(error.message);
-    });
+    countryInput.addEventListener('focusout', validateCountry);
+    cityInput.addEventListener('focusout', validateCity);
+  
+    countryInput.addEventListener('input', renderCityInput);
+  
+    let cityObj = '';
   
     const countryData  = fetch('/js/json/countries.json');
     countryData
-    .then(response => {
-      return response.json();
-     })
-    .then(data => {
-      // console.log(data);
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        for(let key in data) {
+          let countryOption = document.createElement('option');
+          countryOption.innerHTML = data[key];
+          countryOption.value = key;
+          countryInput.appendChild(countryOption);
+        }
+      })
+      .then(setSavedCountry => {
+        if (questionnaire.countryValue) {
+          countryInput.value = questionnaire.countryValue;
+        }
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
   
-    })
-    .catch(error => {
-      console.log(error.message);
-    });
+    const cityData  = fetch('/js/json/cities.json');
+    cityData
+      .then(response => {
+        return response.json();
+      })
+      .then(data => {
+        cityObj = data;
+        if (questionnaire.cityValue) {
+          cityInput.classList.remove('form__input--not-active');
   
+          for(let key in data) {
+            let cityOption = document.createElement('option');
+            cityOption.innerHTML = data[key].name;
+            cityOption.value = key;
+            cityInput.appendChild(cityOption);
+          }
   
+          cityInput.value = questionnaire.cityValue;
+        }
+      })
+      .catch(error => {
+        console.log(error.message);
+      });
   
+    function renderCityInput(el) {
+      let CountryOptionValue = el.target.value;
   
+      while (cityInput.length > 1) {
+        cityInput.removeChild(cityInput.lastChild);
+      }
+      cityInput.classList.remove('form__input--not-active');
   
+      for (let key in cityObj) {
+        if (+cityObj[key].country === +CountryOptionValue) {
+          let cityOption = document.createElement('option');
+          cityOption.innerHTML = cityObj[key].name;
+          cityOption.value = key;
+          cityInput.appendChild(cityOption);
+        }
+      }
+    }
+  
+    function validateCountry(el) {
+      let countryInputText = el.target.options[el.target.selectedIndex].text;
+      let countryInputValue = el.target.value;
+      questionnaire.setInformation('country', countryInputText);
+      questionnaire.setInformation('countryValue', countryInputValue);
+    }
+  
+    function validateCity(el) {
+      let cityInputText = el.target.options[el.target.selectedIndex].text;
+      let cityInputValue = el.target.value;
+      questionnaire.setInformation('city', cityInputText);
+      questionnaire.setInformation('cityValue', cityInputValue);
+    }
   
   }
   
@@ -271,6 +386,8 @@
     let inputOk = rootElement.querySelector('input[name="input_ok"]');
   
     let urlError = rootElement.querySelector('#url-error');
+  
+    fillSavedSocialNetworks();
   
     checkFb.addEventListener('click', showInput);
     checkVk.addEventListener('click', showInput);
@@ -364,6 +481,29 @@
         el.target.classList.remove('form__input--not-valid');
       }
     }
+  
+    function fillSavedSocialNetworks() {
+      if (questionnaire.fb) {
+        checkFb.checked = true;
+        inputFb.value = questionnaire.fb;
+        inputFb.classList.remove('form__input--hidden');
+      }
+      if (questionnaire.vk) {
+        checkVk.checked = true;
+        inputVk.value = questionnaire.vk;
+        inputVk.classList.remove('form__input--hidden');
+      }
+      if (questionnaire.tw) {
+        checkTw.checked = true;
+        inputTw.value = questionnaire.tw;
+        inputTw.classList.remove('form__input--hidden');
+      }
+      if (questionnaire.ok) {
+        checkOk.checked = true;
+        inputOk.value = questionnaire.ok;
+        inputOk.classList.remove('form__input--hidden');
+      }
+    }
   }
   
     function step4() {
@@ -400,7 +540,6 @@
   
 
   const rootElement = document.querySelector('#root');
-
 
   rootElement.innerHTML = templates['step1']();
 
